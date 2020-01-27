6e6bd7a6345f99e9da3f4070f82ba9013bfca8de
Lea Vandamme <you@example.com>
2020-01-27 12:20:12 +0100

    test heroku

---
+++ src/test/java/com/miage/altea/trainer_api/bo/PokemonTest.java
@@ -0,0 +1,14 @@
+package com.miage.altea.trainer_api.bo;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+import javax.persistence.Embeddable;
+
+public class PokemonTest {
+    @Test
+    void pokemon_shouldBeAnEmbeddable(){
+        assertNotNull(Pokemon.class.getAnnotation(Embeddable.class));
+    }
+}
